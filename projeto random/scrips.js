085a4a1fa078b7b9e4fc88ff349877c4fdb35c38
Eduar <geduarjose7@gmail.com>
2024-12-19 13:57:47 -0300

    projetos jakenPo

---
+++ projeto random/scrips.js
@@ -0,0 +1,21 @@
+/*
+-> sotear um numero 
+-> pergar os meus imput
+
+
+
+
+
+
+*/
+
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+    const result = Math.floor(Math.random() * (max - min) + min)
+
+    alert (result)
+
+}
